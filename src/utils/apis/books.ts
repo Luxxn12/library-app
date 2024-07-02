91286fbabd13e633a3cbf970fbd813c3bd38e5d7
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-03 07:43:13 +0800

    feat : add apis
    Deskripsi:
    - add axios with config
    - add books
    - add borrows
    - add users
    
    No Issue

---
+++ src/utils/apis/books.ts
@@ -0,0 +1,33 @@
+import { IPagination, IResponse } from "@/utils/types/api";
+import { IBook } from "@/utils/types/books";
+import axiosWithConfig from "./axios-with-config";
+
+export const getBooks = async () => {
+  try {
+    const response = await axiosWithConfig.get("/books");
+
+    return response.data as IResponse<IPagination<IBook[]>>;
+  } catch (error : any) {
+    const { message } = error.response.data;
+
+    throw Error(message);
+  }
+};
+
+export const getDetailBook = async (id_book: number) => {
+  try {
+    const response = await axiosWithConfig.get(`/books/${id_book}`);
+
+    return response.data as IResponse<IBook>;
+  } catch (error: any) {
+    const { message } = error.response.data;
+
+    throw Error(message);
+  }
+};
+
+export const postBook = () => {};
+
+export const editBook = () => {};
+
+export const deleteBook = () => {};
