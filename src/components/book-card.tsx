685f14c96b4959493d63695eded07d728862e883
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-10 10:04:47 +0800

    feat : add component
    Deskripsi:
    - add carousel book
    - add custom form field
    - update layout
    - update navbar
    - update book card
    
    No issue

--- src/components/book-card.tsx
+++ src/components/book-card.tsx
@@ -1,35 +1,82 @@
-import { IBook } from "@/utils/types/books";
+// import { IBook } from "@/utils/types/books";
 
-interface Props {
+// interface Props {
+//   data: IBook;
+//   navigate: string;
+//   "data-testid"?: string;
+// }
+
+// const BookCard = (props: Props) => {
+//   const { data, navigate } = props;
+
+//   return (
+//     <a
+//       className="flex flex-col p-4 w-48 md:w-56 lg:w-64 h-fit items-center gap-3"
+//       href={navigate}
+//       data-testid={props["data-testid"]}
+//     >
+//       <figure className="overflow-hidden shadow-md shadow-neutral-300">
+//         <img
+//           className="h-auto w-auto object-cover aspect-[3/4]"
+//           src={data.cover_image}
+//           alt={data.title}
+//           width={250}
+//           height={330}
+//         />
+//       </figure>
+//       <p className="font-bold text-lg tracking-wide text-center">
+//         {data.title}
+//       </p>
+//       <p className="text-muted-foreground text-sm text-center">{data.author}</p>
+//     </a>
+//   );
+// };
+
+// export default BookCard;
+
+import { Link } from "react-router-dom";
+
+
+import { IBook } from "@/utils/types/books";
+import { Skeleton } from "./ui/skeleton";
+
+interface BookCardProps {
   data: IBook;
   navigate: string;
   "data-testid"?: string;
 }
 
-const BookCard = (props: Props) => {
+export const BookCard = (props: BookCardProps) => {
   const { data, navigate } = props;
-
   return (
-    <a
+    <Link
       className="flex flex-col p-4 w-48 md:w-56 lg:w-64 h-fit items-center gap-3"
-      href={navigate}
+      to={navigate}
       data-testid={props["data-testid"]}
     >
-      <figure className="overflow-hidden shadow-md shadow-neutral-300">
+      <figure className="overflow-hidden shadow-md rounded-xl shadow-neutral-300">
         <img
-          className="h-auto w-auto object-cover aspect-[3/4]"
+          className="h-auto w-auto rounded-xl object-cover aspect-[3/4]"
           src={data.cover_image}
           alt={data.title}
           width={250}
           height={330}
         />
       </figure>
-      <p className="font-bold text-lg tracking-wide text-center">
+      <p className="font-bold text-lg tracking-wide text-center text-red-700">
         {data.title}
       </p>
       <p className="text-muted-foreground text-sm text-center">{data.author}</p>
-    </a>
+    </Link>
   );
 };
 
-export default BookCard;
+export const BookCardLoading = () => {
+  return (
+    <div className="flex flex-col p-4 w-48 md:w-56 lg:w-64 h-fit items-center gap-3">
+      <Skeleton className="w-full h-[17rem]" />
+      <Skeleton className="h-4 w-full" />
+      <Skeleton className="h-4 w-full" />
+    </div>
+  );
+};
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,23 @@
+import { useLoaderData } from "react-router-dom";
 import Navbar from "./navbar";
+import useTitle from "@/utils/hooks/use-title";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function Layout(props: Props) {
+  const loaderData = useLoaderData() as string;
+  useTitle(loaderData);
   return (
     <>
-      <div className="w-full h-screen bg-white dark:bg-black text-gray-900 dark:text-white overflow-auto font-roboto flex flex-col items-center">
-        <Navbar />
-        <div>{props.children}</div>
+      <div className="w-full  h-dvh bg-white  overflow-auto font-roboto">
+        <div className=" h-full overflow-auto flex flex-col mx-auto">
+          <Navbar />
+          <div className="w-full flex-1 flex flex-col relative">
+            <div>{props.children}</div>
+          </div>
+        </div>
       </div>
     </>
   );
