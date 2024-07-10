5625738cacec3ce8f7fe2009840014d37e9c178d
lukman <lukman@lukmans-MacBook-Air.local>
2024-07-10 10:05:46 +0800

    feat : update login and register

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,3 +1,112 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+import { Input } from "@/components/ui/input";
+
+import { LoginSchema, loginSchema } from "@/utils/types/auth";
+import { useToken } from "@/utils/contexts/token";
+import { userLogin } from "@/utils/apis/auth";
+import Layout from "@/components/layout";
+import { Form } from "@/components/ui/form";
+import { CustomFormField } from "@/components/custom-formfield";
+import { ButtonLogin } from "@/components/ui/button-login";
+
 export default function Login() {
-  return <div>Login</div>;
+  const { changeToken } = useToken();
+  const navigate = useNavigate();
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: LoginSchema) {
+    try {
+      const response = await userLogin(data);
+
+      changeToken(response.payload.token);
+      toast.success(response.message);
+      navigate("/");
+    } catch (error) {
+      toast.error((error as Error).message);
+    }
+  }
+
+  return (
+    <Layout>
+        <div className="m-auto w-full max-w-md space-y-6 ">
+          <div>
+            <h1 className="text-2xl text-gray-900 font-bold tracking-tight text-foreground sm:text-3xl">
+              Log in to your Account
+            </h1>
+          </div>
+          <Form {...form}>
+            <form
+              data-testid="form-login"
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-4"
+            >
+              <CustomFormField
+                control={form.control}
+                name="email"
+                label="Email"
+              >
+                {(field) => (
+                  <Input
+                    data-testid="input-email"
+                    placeholder="johndoe@mail.com"
+                    type="email"
+                    disabled={form.formState.isSubmitting}
+                    aria-disabled={form.formState.isSubmitting}
+                    {...field}
+                  />
+                )}
+              </CustomFormField>
+              <CustomFormField
+                control={form.control}
+                name="password"
+                label="Password"
+              >
+                {(field) => (
+                  <Input
+                    data-testid="input-password"
+                    placeholder="Password"
+                    type="password"
+                    disabled={form.formState.isSubmitting}
+                    aria-disabled={form.formState.isSubmitting}
+                    {...field}
+                  />
+                )}
+              </CustomFormField>
+              <div className="pt-5">
+                <ButtonLogin
+                  data-testid="btn-submit"
+                  type="submit"
+                  className="w-full"
+                  disabled={form.formState.isSubmitting}
+                  aria-disabled={form.formState.isSubmitting}
+                >
+                  Sign in
+                </ButtonLogin>
+              </div>
+              <div className="text-center">
+                <text className="mt-2 text-muted-foreground">
+                  Don't have an account?{" "}
+                  <Link
+                    to="/register"
+                    className="font-medium text-black hover:underline"
+                  >
+                    Sign up
+                  </Link>
+                </text>
+              </div>
+            </form>
+          </Form>
+        </div>
+    </Layout>
+  );
 }
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,3 +1,170 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+import { CustomFormField } from "@/components/custom-formfield";
+import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import Layout from "@/components/layout";
+
+import { RegisterSchema, registerSchema } from "@/utils/types/auth";
+import { userRegister } from "@/utils/apis/auth";
+import { ButtonLogin } from "@/components/ui/button-login";
+
 export default function Register() {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const form = useForm<RegisterSchema>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      full_name: "",
+      email: "",
+      password: "",
+      repassword: "",
+      address: "",
+      phone_number: "",
+    },
+  });
+
+  async function onSubmit(data: RegisterSchema) {
+    try {
+      const response = await userRegister(data);
+
+      toast.success(response.message);
+      navigate("/login");
+    } catch (error) {
+      toast.error((error as Error).message);
+    }
+  }
+
+  return (
+    <Layout>
+      <div className="m-auto w-full max-w-md space-y-6">
+        <div>
+          <h1 className="text-2xl text-gray-900 font-bold tracking-tight text-foreground sm:text-3xl">
+            Sign up to your account
+          </h1>
+        </div>
+        <Form {...form}>
+          <form
+            data-testid="form-login"
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4"
+          >
+            <CustomFormField
+              control={form.control}
+              name="full_name"
+              label="Full Name"
+            >
+              {(field) => (
+                <Input
+                  data-testid="input-full-name"
+                  placeholder="John Doe"
+                  disabled={form.formState.isSubmitting}
+                  aria-disabled={form.formState.isSubmitting}
+                  {...field}
+                />
+              )}
+            </CustomFormField>
+            <CustomFormField control={form.control} name="email" label="Email">
+              {(field) => (
+                <Input
+                  data-testid="input-email"
+                  placeholder="johndoe@mail.com"
+                  type="email"
+                  disabled={form.formState.isSubmitting}
+                  aria-disabled={form.formState.isSubmitting}
+                  {...field}
+                />
+              )}
+            </CustomFormField>
+            <CustomFormField
+              control={form.control}
+              name="password"
+              label="Password"
+            >
+              {(field) => (
+                <Input
+                  data-testid="input-password"
+                  placeholder="Password"
+                  type="password"
+                  disabled={form.formState.isSubmitting}
+                  aria-disabled={form.formState.isSubmitting}
+                  {...field}
+                />
+              )}
+            </CustomFormField>
+            <CustomFormField
+              control={form.control}
+              name="repassword"
+              label="Retype Password"
+            >
+              {(field) => (
+                <Input
+                  data-testid="input-repassword"
+                  placeholder="Retype Password"
+                  type="password"
+                  disabled={form.formState.isSubmitting}
+                  aria-disabled={form.formState.isSubmitting}
+                  {...field}
+                />
+              )}
+            </CustomFormField>
+            <CustomFormField
+              control={form.control}
+              name="address"
+              label="Address"
+            >
+              {(field) => (
+                <Input
+                  data-testid="input-address"
+                  placeholder="Lorem Ipsum Street"
+                  disabled={form.formState.isSubmitting}
+                  aria-disabled={form.formState.isSubmitting}
+                  {...field}
+                />
+              )}
+            </CustomFormField>
+            <CustomFormField
+              control={form.control}
+              name="phone_number"
+              label="Phone Number"
+            >
+              {(field) => (
+                <Input
+                  data-testid="input-phone-number"
+                  placeholder="+628xxxxxxxx"
+                  disabled={form.formState.isSubmitting}
+                  aria-disabled={form.formState.isSubmitting}
+                  {...field}
+                />
+              )}
+            </CustomFormField>
+            <div className="pt-5">
+              <ButtonLogin
+                data-testid="btn-submit"
+                type="submit"
+                className="w-full"
+                disabled={form.formState.isSubmitting}
+                aria-disabled={form.formState.isSubmitting}
+              >
+                Sign up
+              </ButtonLogin>
+            </div>
+            <div className="text-center">
+              <text className="mt-2 text-muted-foreground">
+                Don't have an account?{" "}
+                <Link
+                  to="/login"
+                  className="font-medium text-black hover:underline"
+                >
+                  Sign in
+                </Link>
+              </text>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </Layout>
+  );
 }
